04eb38ad49e5522e7a7fe3322eac45ecd33cceed
kevintausa <61163963+kevintausa@users.noreply.github.com>
2023-05-27 00:47:22 -0500

    Revert "Sprint01 kevin"

--- src/components/navegation/Sidebar.tsx
+++ src/components/navegation/Sidebar.tsx
@@ -7,7 +7,7 @@ export default function Sidebar() {
 
                 <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                     <svg className="bi me-2" width="40" height="32"><use></use></svg>
-                    <span className="fs-4">Mansion Rosa</span>
+                    <span className="fs-4">Sidebar</span>
                 </a>
                 <hr />
                 <ul className="nav nav-pills flex-column mb-auto" style={{minHeight:"62vh"}}>
@@ -26,25 +26,19 @@ export default function Sidebar() {
                     <li>
                         <a href="#" className="nav-link text-white">
                             <svg className="bi me-2" width="16" height="16"><use></use></svg>
-                            Empleados
+                            Orders
                         </a>
                     </li>
                     <li>
                         <a href="#" className="nav-link text-white">
                             <svg className="bi me-2" width="16" height="16"><use></use></svg>
-                            Ventas
+                            Products
                         </a>
                     </li>
                     <li>
                         <a href="#" className="nav-link text-white">
                             <svg className="bi me-2" width="16" height="16"><use></use></svg>
-                            Agenda
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="nav-link text-white">
-                            <svg className="bi me-2" width="16" height="16"><use></use></svg>
-                            Turnos
+                            Customers
                         </a>
                     </li>
                 </ul>
--- src/routes.tsx
+++ src/routes.tsx
@@ -5,7 +5,7 @@ import {
 import Auth from './components/Login/Auth';
 import Dashboard from './components/dashboard/Dashboard';
 import NotFound from './components/errors/NotFound';
-import Empleados from './components/empleados/Empleados';
+import Empleados from './components/dashboard/Empleados';
 const App = lazy(()=> import('./App'));
 
 const routesPublic = createBrowserRouter ([
